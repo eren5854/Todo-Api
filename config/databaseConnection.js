9d20a86402488f5a681da8cddda4f9bfa0902d9c
eren5854 <ihsanerendelibas@hotmail.com>
2023-10-26 00:46:07 +0300

    .

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 
-mongoose.connect("mongodb+srv://test:1@cv-v3-db.5bxwt3d.mongodb.net/", {
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
